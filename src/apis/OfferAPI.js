377054679cf3881df96484b4c0aef6a897990d27
Tito Mathews <tito.mathews@gmail.com>
2021-01-04 16:03:46 -0700

    point to mock service

--- src/apis/OfferAPI.js
+++ src/apis/OfferAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:3000/",
+    /*baseURL: "http://localhost:3000/",*/
+    baseURL: "https://mpp-mock-svc-asys.cf-ssb-z3-dev.discoverfinancial.com/",
     responseType: "json"
 });
