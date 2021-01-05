91178d95b276fdc0398bb8d16d058d5d00bf0589
Tito Mathews <tito.mathews@gmail.com>
2021-01-05 08:44:21 -0700

    Updated

--- src/apis/OfferAPI.js
+++ src/apis/OfferAPI.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     /*baseURL: "http://localhost:3000/",*/
-    baseURL: "https://mpp-mock-svc-asys.cf-ssb-z3-dev.discoverfinancial.com/",
+    baseURL: "https://mpp-rewards-asys.cf-ssb-z3-dev.discoverfinancial.com/api/",
     responseType: "json"
 });
